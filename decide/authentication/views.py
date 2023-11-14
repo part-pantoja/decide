3d9c04c4914f7fdccee2cd5d40f2e3fb6362dea3
dancorfon <dancorfon@alum.us.es>
2023-11-14 13:40:29 +0100

    feat: Implementacion Registro y Login con API

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -10,13 +10,57 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from rest_framework import status
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 from .serializers import UserSerializer
 
 
 
-def login(request):
+def login2(request):
     return render(request, "login.html")
+
+
+
+class GoogleView(TemplateView):
+
+    template_name = 'registro/welcome.html'
+
+    def get_context_data(self, **kwargs):
+        # Obtén el nombre de usuario del usuario autenticado
+        username = self.request.user.username if self.request.user.is_authenticated else None
+
+        # Agrega el nombre de usuario al contexto
+        context = super().get_context_data(**kwargs)
+        context['username'] = username
+        return context
+        
+        
+
+
+
+class LoginView(APIView):
+    def get(self, request):
+        form = AuthenticationForm() 
+        return render(request, 'registro/loginSinGoogle.html', {'form':form})
+    
+    def post(self, request, *args, **kwargs):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            
+            return redirect('bienvenida', username=user.username)
+        else:
+            
+            return render(request, 'registro/loginSinGoogle.html', {'form': form})
+
 
 class GetUserView(APIView):
     def post(self, request):
@@ -38,6 +82,7 @@ class LogoutView(APIView):
 
 
 class RegisterView(APIView):
+    '''
     def post(self, request):
         key = request.data.get('token', '')
         tk = get_object_or_404(Token, key=key)
@@ -57,5 +102,25 @@ class RegisterView(APIView):
         except IntegrityError:
             return Response({}, status=HTTP_400_BAD_REQUEST)
         return Response({'user_pk': user.pk, 'token': token.key}, HTTP_201_CREATED)
-
+    '''
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.data)
+        errors = []
+        if form.is_valid():
+            user = form.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            #return Response({'user_pk': user.pk, 'token': token.key}, status=status.HTTP_201_CREATED)
+            return redirect('bienvenida', username=user.username)
+        else:
+            #return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+            errors = form.errors
+            return render(request, 'registro/registry.html', {'form': form, 'errors': errors})
     
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'registro/registry.html', {'form':form})
+
+class WelcomeView(APIView):
+    def get(self, request, username):
+        return render(request, 'registro/welcome.html', {'username': username})
+
