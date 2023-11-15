c10c80aaf8061ee8246aaa395faae609f1d05de6
andressomoza <andressomozasierra@gmail.com>
2023-11-16 00:28:54 +0100

    feat(booth):add route for /booth in booth/urls.py
    creation of the path to access to /both

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BoothView
+from .views import BoothView, booth_home
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path('', booth_home, name='booth_home'),
     path('<int:voting_id>/', BoothView.as_view()),
 ]
