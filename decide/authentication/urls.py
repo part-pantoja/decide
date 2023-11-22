881e81cc272691876673bbb549218bd60d0a5d8d
eloyjm <eloyjm93@gmail.com>
2023-11-22 10:49:15 +0100

    fix: urls authentication

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -3,10 +3,12 @@ from rest_framework.authtoken.views import obtain_auth_token
 from .views import GetUserView, LogoutView, RegisterView, WelcomeView, LoginView, GoogleView, EmailLoginView, VerifyEmailView
 from django.views.generic import TemplateView
 
+app_name = 'authentication'
+
 
 urlpatterns = [
     path('login/', obtain_auth_token),
-    path('logout/', LogoutView.as_view()),
+    path('logout/', LogoutView.as_view(),name='logout'),
     path('getuser/', GetUserView.as_view()),
     path('register/', RegisterView.as_view(), name='register'),
     path('bienvenida/<str:username>/', WelcomeView.as_view(), name='bienvenida'),
