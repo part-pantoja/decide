9752c30af4711a2457dee3151dbeea41f823f495
bogdanstefan <bogste@alum.us.es>
2023-12-14 15:31:28 +0100

    fix: Cambiar vista #91
    Cambio vista y url de booth para inglés

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BoothView, BoothViewSP, BoothViewDE
+from .views import BoothViewES, BoothViewEN, BoothViewDE
 
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
-    path('<int:voting_id>/sp', BoothViewSP.as_view()),
+    path('<int:voting_id>/es', BoothViewES.as_view()),
+    path('<int:voting_id>/', BoothViewEN.as_view()),
     path('<int:voting_id>/de', BoothViewDE.as_view()),
 ]
--- decide/booth/views.py
+++ decide/booth/views.py
@@ -7,7 +7,7 @@ from base import mods
 
 
 # TODO: check permissions and census
-class BoothView(TemplateView):
+class BoothViewEN(TemplateView):
     template_name = 'booth/booth.html'
 
     def get_context_data(self, **kwargs):
@@ -27,4 +27,50 @@ class BoothView(TemplateView):
 
         context['KEYBITS'] = settings.KEYBITS
 
+        return context
+
+
+class BoothViewES(TemplateView):
+    template_name = 'booth/booth_es.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid = kwargs.get('voting_id', 0)
+
+        try:
+            r = mods.get('voting', params={'id': vid})
+            # Casting numbers to string to manage in javascript with BigInt
+            # and avoid problems with js and big number conversion
+            for k, v in r[0]['pub_key'].items():
+                r[0]['pub_key'][k] = str(v)
+
+            context['voting'] = json.dumps(r[0])
+        except:
+            raise Http404
+        
+        context['KEYBITS'] = settings.KEYBITS
+
+        return context
+    
+
+class BoothViewDE(TemplateView):
+    template_name = 'booth/booth_de.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid = kwargs.get('voting_id', 0)
+
+        try:
+            r = mods.get('voting', params={'id': vid})
+            # Casting numbers to string to manage in javascript with BigInt
+            # and avoid problems with js and big number conversion
+            for k, v in r[0]['pub_key'].items():
+                r[0]['pub_key'][k] = str(v)
+
+            context['voting'] = json.dumps(r[0])
+        except:
+            raise Http404
+        
+        context['KEYBITS'] = settings.KEYBITS
+        
         return context
