8284313f8930362f5d0d8521f428e688be4dc038
bogdanstefan <bogste@alum.us.es>
2023-12-14 13:18:41 +0100

    feat: Creado urls.py #88
    Creado urls.py en alemán (DE) y en español (SP)

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BoothView
+from .views import BoothView, BoothViewSP, BoothViewDE
 
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
+    path('<int:voting_id>/sp', BoothViewSP.as_view()),
+    path('<int:voting_id>/de', BoothViewDE.as_view()),
 ]
