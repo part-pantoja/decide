8e3e4e1d580ded6ab4c1b7c62b2b2aaaf32f165d
bogdanstefan <bogste@alum.us.es>
2023-12-15 02:34:50 +0100

    fix: Añadir url a booth_en #104

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -3,8 +3,8 @@ from .views import BoothViewES, BoothViewEN, BoothViewDE
 
 
 urlpatterns = [
-    path('<int:voting_id>/', BoothView.as_view()),
-    path('<int:voting_id>/es', BoothViewES.as_view()),
     path('<int:voting_id>/', BoothViewEN.as_view()),
+    path('<int:voting_id>/es', BoothViewES.as_view()),
+    path('<int:voting_id>/en', BoothViewEN.as_view()),
     path('<int:voting_id>/de', BoothViewDE.as_view()),
 ]
