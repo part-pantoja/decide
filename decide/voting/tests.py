06dfd253858fe3ca079c3791f393e49f7e0c883a
gonriblun <gonriblun@alum.us.es>
2023-12-16 19:14:03 +0100

    test(voting): testea tally points option

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -224,6 +224,84 @@ class PointsOptionTestCase(StaticLiveServerTestCase):
         
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'alert-success')))
 
+    def test_tally_in_multiple_options_voting(self):
+        q = Question(desc='test question', type = 'points_options', weight = 10)
+        q.save()
+        for i in range(5):
+            opt = QuestionOption(question=q, option='option {}'.format(i+1))
+            opt.save()
+            
+        v = Voting(name='test voting', question=q)
+        v.save()
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+                                          defaults={'me': True, 'name': 'test auth'})
+        a.save()
+        v.auths.add(a)
+        decide_user1 = User.objects.create_user(username='usertest1', password='usertest')
+        decide_user1.save()
+        decide_user2 = User.objects.create_user(username='usertest2', password='usertest')
+        decide_user2.save()
+        c1 = Census(voter_id= decide_user1.id, voting_id=v.id)
+        c2 = Census(voter_id= decide_user2.id, voting_id=v.id)
+        c1.save()
+        c2.save()
+
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+        users = []
+        users.append(decide_user1)
+        users.append(decide_user2)
+
+        for user  in users:
+            self.driver.get(self.live_server_url+"/booth/"+ str(v.id))
+            self.driver.set_window_size(1280, 720)
+            self.driver.find_element(By.CLASS_NAME, 'navbar-toggler').click()
+            self.driver.find_element(By.CLASS_NAME, 'btn-secondary').click()
+            wait = WebDriverWait(self.driver, 10)
+            username_element = wait.until(EC.element_to_be_clickable((By.ID, "username")))
+            username_element.click()
+            self.driver.find_element(By.ID, "username").send_keys(user.username)
+            self.driver.find_element(By.ID, "password").click()
+            self.driver.find_element(By.ID, "password").send_keys("usertest")
+            self.driver.find_element(By.CLASS_NAME, 'btn-primary').click()
+            wait.until(EC.element_to_be_clickable((By.ID, "q2")))
+
+            self.driver.find_element(By.ID, "q2").click()
+            self.driver.find_element(By.ID, "q2").send_keys("4")
+
+            self.driver.find_element(By.ID, "q3").click()
+            self.driver.find_element(By.ID, "q3").send_keys("6")
+            wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'btn-primary')))
+
+            self.driver.find_element(By.CLASS_NAME, 'btn-primary').click()
+        
+        self.driver.get(self.live_server_url+"/admin/login/?next=/admin/")
+        self.driver.set_window_size(1280, 720)
+
+        self.driver.find_element(By.ID, "id_username").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("decide")
+
+        self.driver.find_element(By.ID, "id_password").click()
+        self.driver.find_element(By.ID, "id_password").send_keys("decide")
+
+        self.driver.find_element(By.ID, "id_password").send_keys(Keys.ENTER)
+        self.driver.get(self.live_server_url+"/admin/voting/voting/")
+
+        checkbox = self.driver.find_element(By.XPATH, "//input[@name='_selected_action' and @value='1']")
+        checkbox.click()
+
+        actions_dropdown = Select(self.driver.find_element(By.NAME, 'action'))
+        actions_dropdown.select_by_visible_text('Stop')
+        self.driver.find_element(By.NAME, 'index').click()
+        WebDriverWait(self.driver, 20)
+
+        actions_dropdown = Select(self.driver.find_element(By.NAME, 'action'))
+        actions_dropdown.select_by_visible_text('Tally')
+        self.driver.find_element(By.NAME, 'index').click()
+        WebDriverWait(self.driver, 20)
+        self.assertTrue(self.driver.current_url == self.live_server_url+"/admin/voting/voting/")
+
 class VotingTestCase(BaseTestCase):
     
     def setUp(self):
