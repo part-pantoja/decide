5173d132bb6ba8993b4a029d6fa7714d43f848ea
jaime-carbo <jaicarsan1@alum.us.es>
2023-12-17 17:29:54 +0100

    tipo: asunto #id
    cuerpo

--- decide/voting/test_selem.py
+++ decide/voting/test_selem.py
@@ -5,9 +5,6 @@ from base.tests import BaseTestCase
 from django.utils import timezone
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import time
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -20,36 +17,14 @@ from census.models import Census
 
 from mixnet.models import Auth
 from voting.models import Voting, Question, QuestionOption
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from base import mods
 from base.tests import BaseTestCase
-from census.models import Census
-from mixnet.mixcrypt import ElGamal
-from mixnet.mixcrypt import MixCrypt
 from mixnet.models import Auth
 from voting.models import Voting, Question, QuestionOption
 from datetime import timedelta
 
 from django.contrib.auth.models import User
-
-class AdminTestCase(StaticLiveServerTestCase):
-    def setUp(self):
-        self.base = BaseTestCase()
-        self.base.setUp()
-        options = webdriver.ChromeOptions()
-        options.add_argument("--no-sandbox")
-        options.headless = True
-        self.driver = webdriver.Chrome(options=options)
-        User.objects.create_superuser('admin1', 'admin@example.com', 'admin')
-        super().setUp()
-    def tearDown(self):
-        super().tearDown()
-        self.driver.quit()
-
-        self.base.tearDown()
 
 class MultipleOptionTestCase(StaticLiveServerTestCase):
 
@@ -61,6 +36,7 @@ class MultipleOptionTestCase(StaticLiveServerTestCase):
         options.headless = True
         self.driver = webdriver.Chrome(options=options)
         self.decide_user = User.objects.create_user(username='decide', password='decide')
+        User.objects.create_superuser('admin1', 'admin@example.com', 'admin')
         self.decide_user.is_staff = True
         self.decide_user.is_superuser = True
         self.decide_user.save()
@@ -71,6 +47,112 @@ class MultipleOptionTestCase(StaticLiveServerTestCase):
         super().tearDown()
         self.driver.quit()
         self.base.tearDown()
+
+    def test_preguntaconvotoenblanco(self):
+        self.driver.get(self.live_server_url+"/admin/login/?next=/admin/")
+        self.driver.implicitly_wait(30)
+        self.driver.set_window_size(1850, 1016)
+        self.driver.find_element(By.ID, "id_username").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("admin1")
+        self.driver.find_element(By.ID, "id_password").send_keys("admin")
+        self.driver.find_element(By.CSS_SELECTOR, ".submit-row > input").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".model-question .addlink").click()
+        self.driver.find_element(By.ID, "id_id").click()
+        self.driver.find_element(By.ID, "id_id").send_keys('12')
+        self.driver.find_element(By.ID, "id_desc").send_keys("pregunta con voto en blanco")
+        self.driver.find_element(By.CSS_SELECTOR, ".vCheckboxLabel").click()
+        self.driver.find_element(By.NAME, "_save").click()
+        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) > .field-__str__ > a").click()
+        self.driver.find_element(By.ID, "id_options-0-DELETE").click()
+        self.driver.find_element(By.ID, "id_options-0-DELETE").click()
+        self.driver.find_element(By.NAME, "_save").click()
+        self.driver.find_element(By.NAME, "_selected_action").click()
+        dropdown = self.driver.find_element(By.NAME, "action")
+        dropdown.find_element(By.XPATH, "//option[. = 'Delete selected questions']").click()
+        element = self.driver.find_element(By.NAME, "action")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).click_and_hold().perform()
+        element = self.driver.find_element(By.NAME, "action")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.NAME, "action")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).release().perform()
+        self.driver.find_element(By.NAME, "index").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(4)").click()
+
+    def test_crear_auth(self):
+        self.driver.get(self.live_server_url+"/admin/login/?next=/admin/")
+        self.driver.set_window_size(1850, 1016)
+        self.driver.find_element(By.ID, "id_username").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("admin1")
+        self.driver.find_element(By.ID, "id_password").send_keys("admin")
+        self.driver.find_element(By.CSS_SELECTOR, ".submit-row > input").click()
+        self.driver.find_element(By.LINK_TEXT, "Auths").click()
+        self.driver.find_element(By.CSS_SELECTOR, "li > .addlink").click()
+        self.driver.find_element(By.ID, "id_name").send_keys("testauth")
+        self.driver.find_element(By.ID, "id_url").click()
+        self.driver.find_element(By.ID, "id_url").send_keys("http://localhost:8080")
+        self.driver.find_element(By.ID, "id_me").click()
+        self.driver.find_element(By.NAME, "_save").click()
+        self.driver.find_element(By.LINK_TEXT, "http://localhost:8080").click()
+        self.driver.find_element(By.LINK_TEXT, "Delete").click()
+        self.driver.find_element(By.ID, "content").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
+
+    def test_crear_votacion(self):
+        self.driver.get(self.live_server_url)
+        self.driver.set_window_size(1387, 752)
+        a, _ = Auth.objects.get_or_create(url=self.live_server_url,
+                                        defaults={'me': True, 'name': 'test auth'})
+        a.save()
+        nombre_pregunta = "test question"
+        q = Question.objects.create(id=1000,desc=nombre_pregunta, type=Question.TypeChoices.MULTIPLE_CHOICE)
+        self.driver.implicitly_wait(30)
+        q.save()
+        self.driver.find_element(By.LINK_TEXT, "Sign In").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("admin1")
+        self.driver.find_element(By.ID, "id_password").send_keys("admin")
+        self.driver.find_element(By.ID, "id_password").send_keys(Keys.ENTER)
+        self.driver.find_element(By.LINK_TEXT, "Create voting").click()
+        self.driver.find_element(By.ID, "id_name").click()
+        self.driver.find_element(By.ID, "id_name").send_keys("votacion")
+        self.driver.find_element(By.ID, "id_desc").send_keys("votación de test")
+        dropdown = self.driver.find_element(By.ID, "id_questions")
+        dropdown.find_element(By.XPATH, "//option[. = '"+nombre_pregunta+"']").click()
+        dropdown = self.driver.find_element(By.ID, "id_auths")
+        dropdown.find_element(By.XPATH, "//option[. = '"+self.live_server_url+"']").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(6)").click()
+
+    def test_agregar_al_censo(self):
+        self.driver.get(self.live_server_url)
+        self.driver.set_window_size(1387, 752)
+        a, _ = Auth.objects.get_or_create(url=self.live_server_url,
+                                        defaults={'me': True, 'name': 'test auth'})
+        a.save()
+        nombre_pregunta = "test question"
+        q = Question.objects.create(id=1000,desc=nombre_pregunta, type=Question.TypeChoices.MULTIPLE_CHOICE)
+        q.save()
+        self.driver.find_element(By.LINK_TEXT, "Sign In").click()
+        self.driver.implicitly_wait(30)
+        self.driver.find_element(By.ID, "id_username").send_keys("admin1")
+        self.driver.find_element(By.ID, "id_password").send_keys("admin")
+        self.driver.find_element(By.ID, "id_password").send_keys(Keys.ENTER)
+        self.driver.find_element(By.LINK_TEXT, "Create voting").click()
+        self.driver.find_element(By.ID, "id_name").click()
+        self.driver.find_element(By.ID, "id_name").send_keys("votacion")
+        self.driver.find_element(By.ID, "id_desc").send_keys("votacion de test")
+        dropdown = self.driver.find_element(By.ID, "id_questions")
+        dropdown.find_element(By.XPATH, "//option[. = '"+nombre_pregunta+"']").click()
+        dropdown = self.driver.find_element(By.ID, "id_auths")
+        dropdown.find_element(By.XPATH, "//option[. = '"+self.live_server_url+"']").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(6)").click()
+        self.driver.find_element(By.LINK_TEXT, "Add to census").click()
+        self.driver.find_element(By.ID, "id_voting_id").click()
+        self.driver.find_element(By.ID, "id_voting_id").send_keys("1")
+        self.driver.find_element(By.ID, "id_voter_id").send_keys("1")
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(4)").click()
 
     def testcreateMultipleOptionQuestionSuccess(self):
 
@@ -392,119 +474,3 @@ class PointsOptionTestCase(StaticLiveServerTestCase):
 
         self.assertTrue(self.driver.current_url == self.live_server_url+"/admin/voting/voting/")
         self.base.tearDown()
-    def test_preguntaconvotoenblanco(self):
-        self.driver.get(self.live_server_url+"/admin/login/?next=/admin/")
-        self.driver.set_window_size(1850, 1016)
-        self.driver.find_element(By.ID, "id_username").click()
-        self.driver.find_element(By.ID, "id_username").send_keys("admin1")
-        self.driver.find_element(By.ID, "id_password").send_keys("admin")
-        self.driver.find_element(By.CSS_SELECTOR, ".submit-row > input").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".model-question .addlink").click()
-        self.driver.find_element(By.ID, "id_desc").send_keys("pregunta con voto en blanco")
-        self.driver.find_element(By.CSS_SELECTOR, ".vCheckboxLabel").click()
-        self.driver.find_element(By.NAME, "_save").click()
-        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) > .field-__str__ > a").click()
-        self.driver.find_element(By.ID, "id_options-0-DELETE").click()
-        self.driver.find_element(By.ID, "id_options-0-DELETE").click()
-        self.driver.find_element(By.NAME, "_save").click()
-        self.driver.find_element(By.NAME, "_selected_action").click()
-        dropdown = self.driver.find_element(By.NAME, "action")
-        dropdown.find_element(By.XPATH, "//option[. = 'Delete selected questions']").click()
-        element = self.driver.find_element(By.NAME, "action")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).click_and_hold().perform()
-        element = self.driver.find_element(By.NAME, "action")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).perform()
-        element = self.driver.find_element(By.NAME, "action")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).release().perform()
-        self.driver.find_element(By.NAME, "index").click()
-        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(4)").click()
-
-    def test_crear_auth(self):
-        self.driver.get(self.live_server_url+"/admin/login/?next=/admin/")
-        self.driver.set_window_size(1850, 1016)
-        self.driver.find_element(By.ID, "id_username").click()
-        self.driver.find_element(By.ID, "id_username").send_keys("admin1")
-        self.driver.find_element(By.ID, "id_password").send_keys("admin")
-        self.driver.find_element(By.CSS_SELECTOR, ".submit-row > input").click()
-        self.driver.find_element(By.LINK_TEXT, "Auths").click()
-        self.driver.find_element(By.CSS_SELECTOR, "li > .addlink").click()
-        self.driver.find_element(By.ID, "id_name").send_keys("testauth")
-        self.driver.find_element(By.ID, "id_url").click()
-        self.driver.find_element(By.ID, "id_url").send_keys("http://localhost:8080")
-        self.driver.find_element(By.ID, "id_me").click()
-        self.driver.find_element(By.NAME, "_save").click()
-        self.driver.find_element(By.LINK_TEXT, "http://localhost:8080").click()
-        self.driver.find_element(By.LINK_TEXT, "Delete").click()
-        self.driver.find_element(By.ID, "content").click()
-        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
-
-    def test_crear_votacion(self):
-        self.driver.get(self.live_server_url)
-        self.driver.set_window_size(1387, 752)
-        a, _ = Auth.objects.get_or_create(url=self.live_server_url,
-                                        defaults={'me': True, 'name': 'test auth'})
-        a.save()
-        nombre_pregunta = "test question"
-        q = Question(desc=nombre_pregunta)
-        q.save()
-        self.driver.find_element(By.LINK_TEXT, "Sign In").click()
-        self.driver.find_element(By.ID, "id_username").send_keys("admin1")
-        self.driver.find_element(By.ID, "id_password").send_keys("admin")
-        self.driver.find_element(By.ID, "id_password").send_keys(Keys.ENTER)
-        self.driver.find_element(By.LINK_TEXT, "Create voting").click()
-        self.driver.find_element(By.ID, "id_name").click()
-        self.driver.find_element(By.ID, "id_name").send_keys("votacion")
-        self.driver.find_element(By.ID, "id_desc").send_keys("votación de test")
-        dropdown = self.driver.find_element(By.ID, "id_question")
-        dropdown.find_element(By.XPATH, "//option[. = '"+nombre_pregunta+"']").click()
-        element = self.driver.find_element(By.ID, "id_question")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).click_and_hold().perform()
-        element = self.driver.find_element(By.ID, "id_question")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).perform()
-        element = self.driver.find_element(By.ID, "id_question")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).release().perform()
-        dropdown = self.driver.find_element(By.ID, "id_auths")
-        dropdown.find_element(By.XPATH, "//option[. = '"+self.live_server_url+"']").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(6)").click()
-
-    def test_agregar_al_censo(self):
-        self.driver.get(self.live_server_url)
-        self.driver.set_window_size(1387, 752)
-        a, _ = Auth.objects.get_or_create(url=self.live_server_url,
-                                        defaults={'me': True, 'name': 'test auth'})
-        a.save()
-        nombre_pregunta = "test question"
-        q = Question(desc=nombre_pregunta)
-        q.save()
-        self.driver.find_element(By.LINK_TEXT, "Sign In").click()
-        self.driver.find_element(By.ID, "id_username").send_keys("admin1")
-        self.driver.find_element(By.ID, "id_password").send_keys("admin")
-        self.driver.find_element(By.ID, "id_password").send_keys(Keys.ENTER)
-        self.driver.find_element(By.LINK_TEXT, "Create voting").click()
-        self.driver.find_element(By.ID, "id_name").click()
-        self.driver.find_element(By.ID, "id_name").send_keys("votacion")
-        self.driver.find_element(By.ID, "id_desc").send_keys("votacion de test")
-        dropdown = self.driver.find_element(By.ID, "id_question")
-        dropdown.find_element(By.XPATH, "//option[. = '"+nombre_pregunta+"']").click()
-        element = self.driver.find_element(By.ID, "id_question")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).click_and_hold().perform()
-        element = self.driver.find_element(By.ID, "id_question")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).perform()
-        dropdown = self.driver.find_element(By.ID, "id_auths")
-        dropdown.find_element(By.XPATH, "//option[. = '"+self.live_server_url+"']").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(6)").click()
-        self.driver.find_element(By.LINK_TEXT, "Add to census").click()
-        self.driver.find_element(By.ID, "id_voting_id").click()
-        self.driver.find_element(By.ID, "id_voting_id").send_keys("1")
-        self.driver.find_element(By.ID, "id_voter_id").send_keys("1")
-        self.driver.implicitly_wait(10)
-        self.driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(4)").click()
-  
