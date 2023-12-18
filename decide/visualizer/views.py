58ae1fd9fb44d43d5ffa5eaf985c33e1a385e551
bogdanstefan <bogste@alum.us.es>
2023-12-18 01:09:03 +0100

    fix: Visualizer terminado #151
    Cuerpo del commit

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -20,3 +20,33 @@ class VisualizerView(TemplateView):
             raise Http404
 
         return context
+
+class VisualizerViewDE(TemplateView):
+    template_name = 'visualizer/visualizer_de.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid = kwargs.get('voting_id', 0)
+
+        try:
+            r = mods.get('voting', params={'id': vid})
+            context['voting'] = json.dumps(r[0])
+        except:
+            raise Http404
+
+        return context
+
+class VisualizerViewEN(TemplateView):
+    template_name = 'visualizer/visualizer_en.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid = kwargs.get('voting_id', 0)
+
+        try:
+            r = mods.get('voting', params={'id': vid})
+            context['voting'] = json.dumps(r[0])
+        except:
+            raise Http404
+
+        return context
